1c24b813b352ed82f7781b0360d1102ebe9a6641
vishalkrishnads <321vishalds@gmail.com>
2024-08-18 21:11:06 +0530

    refactored cards

--- app/(stack)/index.tsx
+++ app/(stack)/index.tsx
@@ -1,18 +1,13 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import * as Progress from 'react-native-progress';
-import { View, Text, FlatList, Dimensions } from 'react-native';
-import { Image } from 'expo-image';
-import { Link } from 'expo-router';
+import { View, FlatList, Dimensions } from 'react-native';
 import { scale, useOrientation, isTablet } from '@/assets/utils/responsive';
-import { useTheme, blurhash } from '@/assets/utils/theme'
+import { useTheme } from '@/assets/utils/theme'
 import { Movie } from '@/constants/types';
-import Genre from '@/components/Gentre';
 import useStyles from './styles/movieList.styles'
 import { Bar } from '@/components/Search';
-import Popularity from '@/components/Popularity';
-import Rating from '@/components/Rating';
 import { useMovieContext } from '@/contexts/MovieContext';
-import { formatReleaseDate, image } from '@/api';
+import Card from '@/components/Card';
 
 const MovieListScreen = () => {
 
@@ -29,37 +24,6 @@ const MovieListScreen = () => {
             fetchMovies(state.currentFilter, state[state.currentFilter].page + 1).then(() => setLoading(false));
         }
     };
-
-    type CardProps = { movie: Movie; width: number };
-    const Card = React.memo(({ movie, width }: CardProps) => {
-        return (
-            <Link href={`/(stack)/${movie.id}`}>
-                <View style={[styles.movie, { width }]}>
-                    <View style={styles.card}>
-                        <View style={styles.cardPoster}>
-                            <Image
-                                style={styles.poster}
-                                placeholder={theme.isDark ? require('@/assets/images/pacman-dark.gif') : require('@/assets/images/pacman-light.gif')}
-                                source={image(movie.poster_path)}
-                                autoplay
-                            />
-                        </View>
-                        <View style={styles.cardDetails}>
-                            <Text style={styles.title}>{movie.original_title}</Text>
-                            {movie.genre_ids.length > 0 && <Genre genres_id={movie.genre_ids.splice(0, 2)} />}
-                            <Text style={[theme.textVariants.body, { color: 'grey' }]}>Released {formatReleaseDate(movie.release_date)}</Text>
-                            <View style={styles.popularity}>
-                                <Popularity popularity={Math.round(movie.popularity / 100)} />
-                            </View>
-                            <View style={styles.rating}>
-                                <Rating rating={movie.vote_average} />
-                            </View>
-                        </View>
-                    </View>
-                </View>
-            </Link>
-        );
-    });
 
     const screenWidth = Dimensions.get('window').width;
     const getNumColumns = () => {
