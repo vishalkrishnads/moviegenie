2ff708b7c98d3730b69b6eb0c20db45408374a13
vishalkrishnads <321vishalds@gmail.com>
2024-08-18 13:22:25 +0530

    added cache

---
+++ api/cache.ts
@@ -0,0 +1,23 @@
+import { storeData, getData } from "@/assets/utils/cache";
+import { Movie } from "@/constants/types";
+
+export type CacheFormat = {
+    movies: Movie[],
+    page: number,
+    total_pages: number
+}
+
+export const setPopularMovies = (movies: CacheFormat) => storeData({ key: 'popular' }, movies)
+export const getPopularMovies = async(): Promise<CacheFormat | null> => await getData({ key: 'popular' })
+
+export const setUpcomingMovies = (movies: CacheFormat) => storeData({ key: 'upcoming' }, movies)
+export const getUpcomingMovies = async(): Promise<CacheFormat | null> => await getData({ key: 'upcoming' })
+
+export const setTrendingMovies = (movies: CacheFormat) => storeData({ key: 'trending' }, movies)
+export const getTrendingMovies = async(): Promise<CacheFormat | null> => await getData({ key: 'trending' })
+
+export const setNowPlayingMovies = (movies: CacheFormat) => storeData({ key: 'nowPlaying' }, movies)
+export const getNowPlayingMovies = async(): Promise<CacheFormat | null> => await getData({ key: 'nowPlaying' })
+
+export const setTopRatedMovies = (movies: CacheFormat) => storeData({ key: 'topRated' }, movies)
+export const getTopRatedMovies = async(): Promise<CacheFormat | null> => await getData({ key: 'topRated' })
